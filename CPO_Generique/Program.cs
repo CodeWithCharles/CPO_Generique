ac3dba80eddc5893e7d65a208d7f85b62f396435
Kiwi <charles.probe@gmail.com>
2023-12-20 17:23:47 +0100

    Initial Commit

---
+++ CPO_Generique/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CPO_Generique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            double b = 5.84;
+            float c = 1.25f;
+            string str1 = "Hha";
+
+            Affichage(ref a);
+            Affichage(ref b);
+            Affichage(ref c);
+            Affichage(ref str1);
+
+            Console.ReadKey();
+        }
+
+        static void Affichage<T>(ref T objet)
+        {
+            Console.WriteLine($"Affichage de l'objet : {objet}");
+        }
+    }
+}
